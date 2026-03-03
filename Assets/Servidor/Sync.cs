3e3b1caa98fd5fed06723ba9b93a1dc3ef40caff
ceciliaD <cedallagiovanna@gmail.com>
2026-03-03 17:46:55 -0300

    jqebhfa;krdhf

--- Assets/Servidor/Sync.cs
+++ Assets/Servidor/Sync.cs
@@ -80,8 +80,6 @@ public partial class Servidor
             var rb = miPorta.GetComponent<Rigidbody>();
             if (rb != null)
             {
-                rb.linearVelocity = Vector3.zero;
-                rb.angularVelocity = Vector3.zero;
                 rb.isKinematic = true;
                 rb.constraints = RigidbodyConstraints.FreezeAll;
             }

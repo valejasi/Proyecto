bb3cb8057bc38bfb7b386cc4fbe5b5d6658e9527
valejasi <vjasidakis@gmail.com>
2026-03-04 22:23:36 -0300

    aaaa

--- Assets/Servidor/Servidor.cs
+++ Assets/Servidor/Servidor.cs
@@ -93,7 +93,7 @@ public partial class Servidor : MonoBehaviour
     void Update()
     {
         // Host: crear sala
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl))
             StartCoroutine(CreateAndStore());
 
         // Join: escribir código + Enter
